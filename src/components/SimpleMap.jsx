c46a23177959d44cb97cf2e7199423468d386887
Leonardo Celente <leocelente@gmail.com>
2021-03-07 18:18:27 -0300

    Legenda do mapa melhoraada

--- src/components/SimpleMap.jsx
+++ src/components/SimpleMap.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import GoogleMapReact from "google-map-react";
 import { connect } from "react-redux";
 import "../css/marker.css";
-import { Row } from "react-bootstrap";
+import { Row, Table } from "react-bootstrap";
 
 const Marker = ({ count, nome }) => (
     <div className="pin">
@@ -42,15 +42,31 @@ class SimpleMap extends Component {
                 </div>
                 <hr />
                 <h3>Legenda:</h3>
-                <div style={{ color: "black", paddingLeft: "2rem" }}>
-                    {this.props.pratica.propriedades.map((p) => (
-                        <Row>
-                            {p.gps.latitude !== 0.0
-                                ? this.props.pratica.propriedades.indexOf(p) + 1 + ":"
-                                : ""}
-                             {p.nome}
-                        </Row>
-                    ))}
+                <div style={{ color: "black", }}>
+                    <Table size="sm" bordered hover>
+                        <thead>
+                            <tr>
+                                <th>Marcador</th>
+                                <th>Nome da Propriedade</th>
+                            </tr>
+                        </thead>
+                    <tbody>
+                    {this.props.pratica.propriedades.map((p) => {
+                            if (p.gps.latitude !== 0.0)
+                                return (
+                                    <tr>
+                                        <td>
+                                            {this.props.pratica.propriedades.indexOf(
+                                                p
+                                            ) + 1}
+                                        </td>
+                                        <td>{p.nome}</td>
+                                    </tr>
+                                );
+                        })}
+
+                    </tbody>
+                    </Table>
                 </div>
             </>
         );
