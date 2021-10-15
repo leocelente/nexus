a5f48d4a83d2ed9699d3efdf271835d408c2d281
Leonardo Celente <leocelente@gmail.com>
2021-10-15 05:46:31 -0300

    Simplificado, separado check de login

--- src/redux/actions/usuarioActions.js
+++ src/redux/actions/usuarioActions.js
@@ -1,10 +1,17 @@
-import { firebaseLogin, firebaseLogout } from "../../api/firebase/auth";
+import {
+    firebaseLogin,
+    firebaseLogout,
+    isLogged,
+} from "../../api/firebase/auth";
 
 export const action_login = (cred) => async (dispatch) => {
-    console.log("ACTION: ", cred);
     firebaseLogin(cred, dispatch);
 };
 
 export const action_logout = () => async (dispatch) => {
     firebaseLogout(dispatch);
+};
+
+export const is_logged = () => async (dispatch) => {
+    isLogged(dispatch);
 };
