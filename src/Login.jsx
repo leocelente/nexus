876d7ea8bce4444a7928107949b22f635f81dbf9
Leonardo Celente <leocelente@gmail.com>
2021-10-15 05:51:13 -0300

    Pagina de login com form e checagem de autenticação

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,22 +1,57 @@
-import React, { Component, useState } from "react";
-import { action_login, action_logout } from "./redux/actions/usuarioActions";
+import React, { Component } from "react";
+import {
+    action_login,
+    action_logout,
+    is_logged,
+} from "./redux/actions/usuarioActions";
 import { connect } from "react-redux";
 import {
     Button,
+    Card,
     Col,
     Container,
-    FormControl,
-    FormText,
-    InputGroup,
+    Form,
     Row,
+    Table,
 } from "react-bootstrap";
+import Header from "./components/general/Header";
 
 class LoginPage extends Component {
-    render() {
-        const { authData } = this.props;
-        const { usuario, logado } = authData;
+    constructor(props) {
+        super(props);
+        this.props.is_logged();
+        this.state = { user: "", pass: "" };
+    }
 
-        if (logado == false) {
+    setUser(ev) {
+        const v = ev.target.value;
+        this.setState({
+            user: v,
+        });
+    }
+
+    setPass(ev) {
+        const v = ev.target.value;
+        this.setState({
+            pass: v,
+        });
+    }
+
+    handleSubmit(f_event) {
+        f_event.preventDefault();
+        this.props.action_login({
+            email: this.state.user,
+            senha: this.state.pass,
+        });
+    }
+
+    render() {
+        const { usuario, logado } = this.props.authentication;
+
+        if (logado === null) {
+            return <>Checando permissões...</>;
+        }
+        if (logado === false) {
             return (
                 <Container>
                     <Row fluid="true" className="justify-content-md-center">
@@ -29,70 +64,103 @@ class LoginPage extends Component {
                                 color: "black",
                             }}
                         >
-                            <div>
-                                <InputGroup className="mb-3">
-                                    <InputGroup.Prepend>
-                                        <InputGroup.Text id="basic-addon1">
-                                            Usuário
-                                        </InputGroup.Text>
-                                    </InputGroup.Prepend>
-                                    <FormControl aria-describedby="basic-addon1" />
-                                </InputGroup>
-
-                                <InputGroup className="mb-3">
-                                    <InputGroup.Prepend>
-                                        <InputGroup.Text id="basic-addon3">
-                                            Senha
-                                        </InputGroup.Text>
-                                    </InputGroup.Prepend>
-                                    <FormControl
-                                        id="basic-url"
-                                        aria-describedby="basic-addon3"
-                                    />
-                                </InputGroup>
-                                <InputGroup className="mb-3">
-                                    <InputGroup.Prepend>
-                                        <InputGroup.Text id="basic-addon3">
-                                            Propriedade?
-                                        </InputGroup.Text>
-                                    </InputGroup.Prepend>
-                                    <FormControl
-                                        id="basic-url"
-                                        aria-describedby="basic-addon3"
-                                    />
-                                </InputGroup>
-                            </div>
-                            <hr></hr>
-                            <input defaultValue="leocelente@usp.br" />
-                            <Button
-                                onClick={() =>
-                                    this.props.action_login({
-                                        email: "leocelente@usp.br",
-                                        senha: "12345678",
-                                    })
-                                }
+                            <Form
+                                noValidate
+                                onSubmit={(e) => this.handleSubmit(e)}
                             >
-                                Login
-                            </Button>{" "}
-                            ----{" "}
-                            <Button onClick={() => this.props.action_logout()}>
-                                Logout
-                            </Button>
-                            <span style={{ color: "black" }}>
-                                :{usuario.nome}:
-                            </span>
+                                <Form.Group
+                                    as={Col}
+                                    // md="4"
+                                    controlId="controlEmail"
+                                >
+                                    <Form.Label>E-Mail</Form.Label>
+                                    <Form.Control
+                                        required
+                                        type="email"
+                                        placeholder="email"
+                                        defaultValue="leocelente@usp.br"
+                                        onChange={(e) => this.setUser(e)}
+                                    />
+                                    <Form.Control.Feedback></Form.Control.Feedback>
+                                </Form.Group>
+
+                                <Form.Group
+                                    as={Col}
+                                    // md="4"
+                                    controlId="controlPassword"
+                                >
+                                    <Form.Label>Senha</Form.Label>
+                                    <Form.Control
+                                        required
+                                        type="text"
+                                        placeholder="senha"
+                                        defaultValue=""
+                                        onChange={(e) => this.setPass(e)}
+                                    />
+                                    <Form.Control.Feedback></Form.Control.Feedback>
+                                </Form.Group>
+                                <Button type="submit">Login</Button>
+                            </Form>
                         </Col>
                     </Row>
                 </Container>
             );
-        } else return <div>{this.props.children}</div>;
+        } else {
+            return (
+                <div>
+                    <Header />
+                    <Row fluid="true" className="justify-content-md-center">
+                        <Col
+                            lg={10}
+                            style={{
+                                marginTop: "4em",
+                                color: "black",
+                            }}
+                        >
+                            <Card>
+                                <Card.Header>
+                                    <Card.Title>
+                                        <h4>Você está Logado</h4>
+                                    </Card.Title>
+                                </Card.Header>
+                                <Card.Body>
+                                    <Table>
+                                        <tr>
+                                            <td>Email: </td>
+                                            <td>
+                                                {usuario.firebaseUser?.email ||
+                                                    "--"}
+                                            </td>
+                                        </tr>
+                                        {/* <tr><td>Nome: </td> <td></td></tr> */}
+                                    </Table>
+                                </Card.Body>
+                                <Card.Footer>
+                                    <Row className="justify-content-md-end">
+                                        <Button
+                                            onClick={() =>
+                                                this.props.action_logout()
+                                            }
+                                        >
+                                            Sair
+                                        </Button>
+                                    </Row>
+                                </Card.Footer>
+                            </Card>
+                        </Col>
+                    </Row>
+                </div>
+            );
+        }
     }
 }
 
 const mapStateToProps = (state) => ({
-    authData: state.authentication,
+    authentication: state.authentication,
 });
 
-export default connect(mapStateToProps, { action_login, action_logout })(
-    LoginPage
-);
+export default connect(mapStateToProps, {
+    action_login,
+    action_logout,
+    is_logged,
+})(LoginPage);
