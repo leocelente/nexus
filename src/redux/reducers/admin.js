7aadc5e3ee9010652a294207dd0824281e525c1c
Leonardo Celente <leocelente@gmail.com>
2021-10-15 05:48:42 -0300

    reducer da seleção de propriedade

---
+++ src/redux/reducers/admin.js
@@ -0,0 +1,15 @@
+import { Propriedade } from "../../api/models/propriedade";
+import { SELECT_ADM_PROPRIEDADE } from "../actions";
+
+const initialState = {
+    propriedade: new Propriedade(),
+};
+
+export default function (state = initialState, action) {
+    switch (action.type) {
+        case SELECT_ADM_PROPRIEDADE:
+            return { ...state, propriedade: action.payload };
+        default:
+            return state;
+    }
+}
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,10 +3,12 @@ import praticas from "./praticas";
 import indicadores from "./indicadores";
 import propriedades from "./propriedades";
 import authentication from "./usuario";
+import admin from "./admin";
 
 export default combineReducers({
     praticas,
     indicadores,
     propriedades,
     authentication,
+    admin,
 });
