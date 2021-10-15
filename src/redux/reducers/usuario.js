013f6ae86f27f87a3d4765a5ce9d9145fb5f8e6a
Leonardo Celente <leocelente@gmail.com>
2021-10-15 05:50:05 -0300

    reducer aprimorado do estado do usuario

--- src/redux/reducers/usuario.js
+++ src/redux/reducers/usuario.js
@@ -1,18 +1,20 @@
 import { Usuario } from "../../api/models/usuario";
-import { LOGIN, LOGOUT } from "../actions";
+import { LOGIN, LOGOUT, AUTH_CHECK } from "../actions";
 
 const initialState = {
     usuario: new Usuario(),
-    logado: false,
+    logado: null,
 };
 
 export default function (state = initialState, action) {
     switch (action.type) {
         case LOGIN:
-            console.log("USER", action.payload);
-            return { ...state, ...action.payload, logado: true };
+            const usuario = action.payload;
+            return { ...state, usuario, logado: true };
         case LOGOUT:
             return { ...state, usuario: new Usuario({}), logado: false };
+        case AUTH_CHECK:
+            return { ...state, logado: action.payload };
         default:
             return state;
     }
