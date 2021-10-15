bb29f8c8eac823be85e3bb3d72a74159125bfb06
Leonardo Celente <leocelente@gmail.com>
2021-10-15 05:44:32 -0300

    Separado o header em um compoente

---
+++ src/components/general/Header.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import { Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+export default class Header extends Component {
+    render() {
+        return (
+            <Navbar bg="dark">
+                <Navbar.Brand className="d-lg-none">
+                    <>
+                        <Link to="/" style={{ color: "white" }}>
+                            <h2> PLA.CO.P.A.S</h2>
+                        </Link>
+                    </>
+                </Navbar.Brand>
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto ">
+                        {" "}
+                        {/* mx-auto */}
+                        <Link to="/">
+                            <h2
+                                className="d-none d-sm-block"
+                                style={{
+                                    color: "white",
+                                }}
+                            >
+                                Plataforma Cooperativa de Práticas Agrícolas
+                                Sustentaveis
+                            </h2>
+                        </Link>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link>
+                            <Link style={{ color: "white" }} to="/admin">
+                                Admin
+                            </Link>
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        );
+    }
+}
