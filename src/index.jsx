19c00ac75729d1bbf74bce5af2c8e70b175adab8
Leonardo Celente <leocelente@gmail.com>
2021-10-15 05:51:40 -0300

    Uso de rotas para segregar partes da plataforma

--- src/index.jsx
+++ src/index.jsx
@@ -3,16 +3,29 @@ import ReactDOM from "react-dom";
 import "./css/bootstrap.css";
 import "./index.css";
 import App from "./App";
+import Admin from "./pages/admin/Admin";
+
+import { Route, Link, BrowserRouter as Router } from "react-router-dom";
 
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import LoginPage from "./Login";
 
+const routes = (
+    <Router>
+        <div>
+            <Route exact path="/" component={App} />
+            <Route path="/admin" component={Admin} />
+            <Route path="/login" component={LoginPage} />
+        </div>
+    </Router>
+);
+
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
             {/* <LoginPage> */}
-            <App />
+            {routes}
             {/* </LoginPage> */}
         </Provider>
     </React.StrictMode>,
